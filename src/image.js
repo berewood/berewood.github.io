86669710d65eb7a3dc8cd53e4f1c83af4b060f53
berewood <saking@btinternet.com>
2022-04-16 22:13:25 +0100

    chg: Add in images and macmillan picture with carousel

--- src/image.js
+++ src/image.js
@@ -1,47 +1,161 @@
-import React from "react";
+import React, {useState, useEffect, useRef} from "react";
 import {Segment, Header, Modal, Image as SemImage} from "semantic-ui-react";
 import PropTypes from "prop-types";
 
-const ImageExpand = ({src, title}) => (
-    <Modal
-        className="image-modal"
-        basic
-        closeIcon
-        size="fullscreen"
-        trigger={
-            <SemImage src={src} style={{cursor: "pointer"}} />
-        }
-    >
-        <Modal.Content>
-            <SemImage src={src} centered />
-            <Header as="h2" content={title} style={{color: "white", textAlign: "center"}}/>
-        </Modal.Content>
-    </Modal>
-);
+const ImagePreview = ({children}) => {
+    const [open, setOpen] = useState(false);
+    const [index, setIndex] = useState(0);
 
-ImageExpand.propTypes = {
+    const [isDragging, setIsDragging] = useState(false);
+    const dragCoords = useRef(0);
+
+
+    const handleOpen = (index) => {
+        setIndex(index);
+        setOpen(true);
+    };
+
+    const handleNavigate = (event) => {
+        if (event.key == "ArrowRight") {
+            navigate("right");
+        } else if (event.key == "ArrowLeft") {
+            navigate("left");
+        }
+    };
+
+
+    const navigate = (name) => {
+        if (name === "left") {
+            if (index === 0) {
+                setIndex(children.length - 1);
+            } else {
+                setIndex(index - 1);
+            }
+        } else {
+            if (index === (children.length - 1)) {
+                setIndex(0);
+            } else {
+                setIndex(index + 1);
+            }
+        }
+    };
+
+    const handleTouchStart = () => {
+        setIsDragging(true);
+    };
+
+    const handleTouchMove = (event) => {
+        const x = event.touches[0].clientX;
+        if (isDragging) {
+            if (dragCoords.current === 0) {
+                dragCoords.current = x;
+                return;
+            }
+            const offset = x - dragCoords.current;
+            if (Math.abs(offset) > 30) {
+                if (Math.sign(offset) === -1) {
+                    // Going to left
+                    navigate("left");
+                } else {
+                    navigate("right");
+                }
+                setIsDragging(false);
+            }
+        }
+    };
+
+    const handleTouchEnd = () => {
+        setIsDragging(false);
+        dragCoords.current = 0;
+    };
+
+    useEffect(() => {
+        if (open) {
+            document.addEventListener("keydown", handleNavigate);
+        }
+        return () => document.removeEventListener("keydown", handleNavigate);
+    }, [open, index]);
+
+    return (
+        <React.Fragment>
+            <SemImage.Group size="medium" style={{textAlign: "center"}}>
+                {children.map((image, index) => (
+                    <SemImage key={index} src={image.props.src} style={{cursor: "pointer"}} onClick={() => handleOpen(index)} />
+                ))}
+            </SemImage.Group>
+            <Modal
+                basic
+                closeIcon
+                open={open}
+                size="fullscreen"
+                className="image-modal"
+                onClose={() => setOpen(false)}
+            >
+                <Modal.Content>
+                    <button name="left" className="image-navigate left" icon="arrow left" onClick={() => navigate("left")} />
+                    <button name="right" className="image-navigate right" icon="arrow right" onClick={() => navigate("right")} />
+                    <figure>
+                        <SemImage
+                            src={children[index].props.src} centered
+                            draggable={false}
+                            onTouchStart={handleTouchStart}
+                            onTouchMove={handleTouchMove}
+                            onTouchEnd={handleTouchEnd}
+                        />
+                        <figcaption>
+                            <div>
+                                <div className="title">{children[index].props.title}</div>
+                                <div className="counter">{index + 1} of {children.length}</div>
+                            </div>
+                        </figcaption>
+                    </figure>
+                </Modal.Content>
+            </Modal>
+        </React.Fragment>
+    );
+};
+
+ImagePreview.propTypes = {
+    children: PropTypes.array
+};
+
+/* eslint-disable no-unused-vars */
+const Image = ({src, title}) => (
+    null
+);
+/* eslint-enable no-unused-vars */
+
+Image.propTypes = {
     src: PropTypes.string,
     title: PropTypes.string
 };
+
 
 const Images = () => {
     return (
         <Segment className="centered compact">
             <Header as="h2" content="Images" />
-            <SemImage.Group size="medium" style={{textAlign: "center"}}>
-                <ImageExpand src="images/garden.jpg" title="Garden" />
-                <ImageExpand src="images/living_room.jpg" title="Living Room" />
-                <ImageExpand src="images/kitchen.jpg" title="Kitchen" />
-                <ImageExpand src="images/dining_room.jpg" title="Dining Room" />
-                <ImageExpand src="images/bedroom_4.jpg" title="Master Bedroom (with en suite bathroom)" />
-                <ImageExpand src="images/bedroom.jpg" title="Double Bedroom 1 (with en suite bathroom)" />
-                <ImageExpand src="images/bedroom_2.jpg" title="Double Bedroom 2" />
-                <ImageExpand src="images/bedroom_3.jpg" title="Twin Bedroom" />
-            </SemImage.Group>
+            <ImagePreview>
+                <Image src="images/garden.jpg" title="Garden" />
+                <Image src="images/garden_2.jpg" title="Garden" />
+                <Image src="images/woods_1.jpg" title="Garden (Woods)" />
+                <Image src="images/woods_2.jpg" title="Garden (Woods)" />
+                <Image src="images/woods_3.jpg" title="Garden (Woods)" />
+                <Image src="images/garden_2.jpg" title="Garden" />
+                <Image src="images/living_room.jpg" title="Living Room" />
+                <Image src="images/kitchen.jpg" title="Kitchen" />
+                <Image src="images/dining_room.jpg" title="Dining Room" />
+                <Image src="images/bedroom_4.jpg" title="Master Bedroom (with en suite bathroom)" />
+                <Image src="images/bedroom.jpg" title="Double Bedroom 1 (with en suite bathroom)" />
+                <Image src="images/bedroom_2.jpg" title="Double Bedroom 2" />
+                <Image src="images/bedroom_3.jpg" title="Twin Bedroom" />
+                <Image src="images/deer_1.jpg" title="Deer" />
+                <Image src="images/deer_2.jpg" title="Deer" />
+                <Image src="images/deer_3.jpg" title="Deer" />
+            </ImagePreview>
         </Segment>
     );
 };
-
 
 
 export default Images;
